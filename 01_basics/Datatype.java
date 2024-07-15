80a930893df11ce4dc0bed0b928d4daebbc4659f
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-15 10:16:32 +0530

    Datatype program

---
+++ 01_basics/Datatype.java
@@ -0,0 +1,21 @@
+class Datatype {
+    public static void main(String args[]){
+        // Declaration
+        int x;
+        float y;
+        char z;
+        boolean a;
+
+        // Initialization
+        x = 45;
+        y = 225.2343f; // by default, decimal value is double so, it order to get it into float, use f.
+        z = 'd';
+        a = true;
+
+        // priting
+        System.out.println("Int: " + x);
+        System.out.println("Double: " + y);
+        System.out.println("Character: " + z);
+        System.out.println("Boolean: " + a);
+    }    
+}
