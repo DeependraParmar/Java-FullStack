bf4eb8f9e6b07b6765bc23602d2b58453e4f1c7f
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-15 09:51:23 +0530

    Naming Convention in Java

--- 01_basics/main.java
+++ 01_basics/main.java
@@ -1,12 +1,10 @@
-class main{
+class Main{
     public static void main(String[] args){
         System.out.println("Hey there");
         System.out.println("Good Morning all !");
     }
 }
 
-class Test1{}
-class Test2{}
 
 // .class file created is created with the name of the class present in the source code.
 // For manual compilation ----> javac Filename.java
@@ -14,6 +12,8 @@ class Test2{}
 
 
 
+// class Test1{}
+// class Test2{}
 
 // If I have multiple classes in a particular source file, multiple bytecodes are generated for them.
 // But, Byte code which has the main function gets executed.
