71fcf0e88572adc78aad85a7f07d1ffe2ed03318
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-15 09:36:28 +0530

    Multiple class in a single file

--- 01_basics/main.java
+++ 01_basics/main.java
@@ -1,10 +1,19 @@
-class Main{
+class main{
     public static void main(String[] args){
         System.out.println("Hey there");
+        System.out.println("Good Morning all !");
     }
 }
 
+class Test1{}
+class Test2{}
 
 // .class file created is created with the name of the class present in the source code.
 // For manual compilation ----> javac Filename.java
 // For manual run         ----> java ClassName
+
+
+
+
+// If I have multiple classes in a particular source file, multiple bytecodes are generated for them.
+// But, Byte code which has the main function gets executed.
