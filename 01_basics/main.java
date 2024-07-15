fdba95ecb6ceb896e046019228e374c95c6b052e
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-15 09:30:57 +0530

    Added manual compilation steps

--- 01_basics/main.java
+++ 01_basics/main.java
@@ -1,4 +1,4 @@
-class main{
+class Main{
     public static void main(String[] args){
         System.out.println("Hey there");
     }
@@ -6,3 +6,5 @@ class main{
 
 
 // .class file created is created with the name of the class present in the source code.
+// For manual compilation ----> javac Filename.java
+// For manual run         ----> java ClassName
