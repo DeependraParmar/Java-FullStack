af032179804514ad352409dde58128dac1edb694
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-15 09:28:22 +0530

    First program

---
+++ 01_basics/main.java
@@ -0,0 +1,8 @@
+class main{
+    public static void main(String[] args){
+        System.out.println("Hey there");
+    }
+}
+
+
+// .class file created is created with the name of the class present in the source code.
