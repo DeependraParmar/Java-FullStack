cff29c3bac59009633c2c40fa6b7bef5a56ca26b
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-16 10:01:27 +0530

    OOPS implementation, constructor and more

---
+++ 02_oops/src/package2/Main.java
@@ -0,0 +1,10 @@
+package package2;
+
+public class Main {
+    public static void main(String[] args) {
+        Student deependra = new Student();
+        deependra.input();
+        deependra.show();
+    }
+}
+
---
+++ 02_oops/src/package2/Student.java
@@ -0,0 +1,40 @@
+package package2;
+
+import java.util.Scanner;
+
+public class Student {
+    private int roll;
+    private String name;
+    private final float[] marks;
+
+    public Student(){
+        this.marks = new float[5];
+    }
+
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the roll number of the student: ");
+        this.roll = sc.nextInt();
+        System.out.print("Enter the name of the student: ");
+        this.name = sc.next();
+        System.out.println("Enter the marks of the 5 subjects: ");
+
+        for(int i=0; i<5; i++){
+            System.out.print("Marks of subject " + (i+1) + ": ");
+            this.marks[i] = sc.nextFloat();
+        }
+
+        System.out.println();
+    }
+    public void show(){
+        System.out.println("Roll No: " + this.roll);
+        System.out.println("Name: " + this.name);
+        System.out.print("Marks: [");
+        for(int i=0; i<5; i++){
+            System.out.print(marks[i] + ",");
+        }
+        System.out.print("]");
+    }
+}
+
