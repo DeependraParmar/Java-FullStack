eabc51d0f005601a5d2b4b8054362eecf75b9774
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-18 08:59:24 +0530

    Initial commit: day 04

---
+++ 04_oops/src/entities/Person.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Person{
+    private String name;
+    private int age;
+    private String phone;
+
+    public Person(){
+        super();
+    }
+
+    public Person(String name, int age, String phone){
+        super();
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    public void show(){
+        System.out.println();
+        System.out.println("Name is: " + this.name);
+        System.out.println("Age is: " + this.age);
+        System.out.println("Phone number is: " + this.phone);
+        System.out.println();
+    }
+}
---
+++ 04_oops/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import entities.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person obj1 = new Person("Deependra Parmar", 20, "9375727372");
+        obj1.show();
+    }
+}
