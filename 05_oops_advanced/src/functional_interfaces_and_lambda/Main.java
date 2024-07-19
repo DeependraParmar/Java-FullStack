561736c9b7e55352ae2fe43a72a27ae430232678
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-19 11:11:36 +0530

    Today's stuff

--- 05_oops_advanced/src/functional_interfaces_and_lambda/Main.java
+++ 05_oops_advanced/src/functional_interfaces_and_lambda/Main.java
@@ -15,6 +15,9 @@ public class Main {
         /*
         *   If your function is returning something, you can do like
         *   Test t = (a,b) -> a/b; // called as Lambda Expression.
+        *
+        *   Lambda is although an anonymous_class but it does not have seperate bytecode.
+        *   It got attached to the bytecode of the main class its written in.
         **/
 
         Test t2 = (a,b) -> {
