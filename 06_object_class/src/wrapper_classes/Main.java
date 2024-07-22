3b352ec85c8b6a5f390767ed454bf3af1bd90073
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-22 09:17:52 +0530

    More about wrappers

--- 06_object_class/src/wrapper_classes/Main.java
+++ 06_object_class/src/wrapper_classes/Main.java
@@ -7,5 +7,24 @@ public class Main {
 
         // In primitive, null cannot be stored.
         // In order to use null, we need wrapper classes.
+
+
+        // Boxing and unboxing:
+        // Boxing: Putting value manually in the wrapper.
+        // Unboxing: Getting the integer/ float value from the reference of the wrapper.
+
+        // Integer z = new Integer(x.intValue() + y.intValue());
+
+        Integer a = new Integer(45);
+        Integer b = new Integer(20);
+
+        Integer c = a + b; // automatic boxing and unboxing.
+        int d = a + b; // also does automatic boxing and unboxing.
+        System.out.println(c);
+
+
+        // Then why do we need primitives?
+        // Ans: All the operators works on primitives. You cannot do it on wrappers.
+        // Wrappers can be used to keep null while, primitives cannot.
     }
 }
