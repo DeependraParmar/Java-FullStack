2425f8a75634f88c1b9a931b7a564b4baf5ec4d2
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-22 09:08:15 +0530

    Wrapper classes

---
+++ 06_object_class/src/wrapper_classes/Main.java
@@ -0,0 +1,11 @@
+package wrapper_classes;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 45; // Value type
+        Integer i = null; // You can initialise reference of wrapper class with null in order to show the absence of value.
+
+        // In primitive, null cannot be stored.
+        // In order to use null, we need wrapper classes.
+    }
+}
