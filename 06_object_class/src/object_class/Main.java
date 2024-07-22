e9103863929b1a78b8f0c730c5a40ec641f2089b
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-22 09:03:01 +0530

    Object class

---
+++ 06_object_class/src/object_class/Main.java
@@ -0,0 +1,10 @@
+package object_class;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(56, "Deependra Parmar", 87.7f);
+        s1.show();
+    }
+}
+
+// Object Class is by default a Super class of every class
---
+++ 06_object_class/src/object_class/Student.java
@@ -0,0 +1,19 @@
+package object_class;
+
+public class Student {
+    private int roll_no;
+    private String name;
+    private float marks;
+
+    public Student(int roll_no, String name, float marks) {
+        this.roll_no = roll_no;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public void show(){
+        System.out.println("Roll no of student is: " + this.roll_no);
+        System.out.println("Name of the student is: " + this.name);
+        System.out.println("Marks of the student is: " + this.marks);
+    }
+}
