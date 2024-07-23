0586776cd843c090c69da4722817c4c4448f29be
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-23 08:45:19 +0530

    Initial commit: Day 07

---
+++ 07_java_classes/src/strings/Main.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hey there !!";
+        System.out.println(str);
+        System.out.println(str.hashCode());
+
+        str = "Hello there !!";
+        System.out.println(str);
+        System.out.println(str.hashCode());
+    }
+}
