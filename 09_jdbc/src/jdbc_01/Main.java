6081d1356d744a47b9d35bae972724edef7a09a4
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-30 09:54:32 +0530

    Fetching data and priting it

--- 09_jdbc/src/jdbc_01/Main.java
+++ 09_jdbc/src/jdbc_01/Main.java
@@ -1,8 +1,6 @@
 package jdbc_01;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +9,34 @@ public class Main {
         final String PASSWORD = "root";
 
         try{
+            // CONNECTION BUILD: Making a connection with the database using JDBC
             Connection connection = DriverManager.getConnection(CONNECTION_URL, ID, PASSWORD);
             System.out.println("Connected to Database....");
+            System.out.println();
+
+            // STATEMENT BUILD: If you want to execute a query, you need Statement Class in JDBC
+            Statement statement = connection.createStatement();
+
+            // EXECUTING QUERY: Executing the Query using Statement.
+            ResultSet resultSet = statement.executeQuery("select * from student");
+
+            // PRINTING RECORDS: Printing the rows
+            while(resultSet.next()){
+                // accessing columns via number of column: Not a great choice
+//                int rollno = resultSet.getInt(1);
+//                String name = resultSet.getString(2);
+//                int age = resultSet.getInt(3);
+//                String email = resultSet.getString(4);
+//                float marks = resultSet.getFloat(5);
+
+                int rollno = resultSet.getInt("rollno");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String email = resultSet.getString("email");
+                float marks = resultSet.getFloat("marks");
+
+                System.out.println(rollno + " " + name + " " + age + " " + email + " " + marks);
+            }
         }
         catch(SQLException e){
             System.out.println("Error Connecting to Database....");
