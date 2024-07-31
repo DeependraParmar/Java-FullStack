354be453bf552c4112126cae184e5201226ca104
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-31 10:22:10 +0530

    Added object-relation mapping in Hibernate

--- 10_hibernate_maven/src/main/java/com/ormdemo/Student.java
+++ 10_hibernate_maven/src/main/java/com/ormdemo/Student.java
@@ -1,10 +1,28 @@
 package com.ormdemo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "student") // optional if name of the class is same as Student & Student.
+
 public class Student {
+    @Id
+    @Column(name = "rollno") // optional if name of data-member and column is same.
     private int rollno;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "age")
     private int age;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "marks")
     private float marks;
 
     public Student(){}
