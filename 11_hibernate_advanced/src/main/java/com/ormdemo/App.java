750b56d61118d32c8bdd7189527413aa71337942
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-01 10:57:45 +0530

    Most Important Logs and instructions: Theory for Hibernate, Cache, Persistent objects'

--- 11_hibernate_advanced/src/main/java/com/ormdemo/App.java
+++ 11_hibernate_advanced/src/main/java/com/ormdemo/App.java
@@ -27,7 +27,7 @@ public class App
 //        employeeService.updateEmployee(e);
 
 //        Deleting the record
-        employeeService.deleteEmployee(35);
+//        employeeService.deleteEmployee(35);
 
         SessionFactoryBuilder.closeFactory();
     }
@@ -40,4 +40,41 @@ public class App
             you to create a default constructor.
         2. You may have null values in the columns in the db but not in the primitives.
             So, always use Wrapper class which can store both null and not-null values.
+*/
+
+
+
+/*
+    Important Points to Ponder:
+        1. When you get the session from sessionFactory.openSession(), a cache is created
+            and is destroyed when session.close();
+        2. When you have some objects in the Java Program and provide it to hibernate,
+            hibernate stores it in the cache and these objects are called as Persistent
+            Objects which persists until cache is persistent.
+        3. For making an object as Persistent Object, you call save() method which doesn't
+            save directly to db. It is just stored in the cache.
+        4. If you do session.save(emp) and then session.close(), it was first saved to cache and
+            destroyed the cache but ultimately not saved to the database.
+        5. Object is not saved to the db, until you do session.beginTransaction().commit();
+        6. If once saved, and you changed thereafter like belo
+            session.save(emp);
+            emp.setAge(25);
+            commit();
+
+            followup: user will have age of 25 despite 20.
+
+
+        7. Now, let's see how data/records come from db to you.
+        8. Firstly, when you call get(), all objects fetched are made persistent objects.
+
+
+        MORAL OF THE STORY
+
+        Whenever you change persistent objects and commits it(), changes are directly made
+        into the databases.
+
+        One Stop solution to remove a persistent object from cache even letting the session
+        open is session.detach(emp); -----> Now, making changes and committing will not
+        get reflected to the database.
+
 */
