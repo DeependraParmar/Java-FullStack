2b56a715bfe3a5c193e0cd09941be27312606622
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-01 10:02:49 +0530

    Added tips and logs in the main APP

--- 11_hibernate_advanced/src/main/java/com/ormdemo/App.java
+++ 11_hibernate_advanced/src/main/java/com/ormdemo/App.java
@@ -19,3 +19,12 @@ public class App
         SessionFactoryBuilder.closeFactory();
     }
 }
+
+/*
+    Points to remember while working with Java and Hibernate
+        1. When, you fetch a single record from the db and return it, hibernate creates
+            object of the class from the "DEFAULT CONSTRUCTOR" so, it is mandatory for
+            you to create a default constructor.
+        2. You may have null values in the columns in the db but not in the primitives.
+            So, always use Wrapper class which can store both null and not-null values.
+*/
