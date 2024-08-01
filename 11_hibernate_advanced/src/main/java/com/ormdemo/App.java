9c3e7a5d758dd88f89c58e58698eef1a7fdc1b2e
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-01 09:07:26 +0530

    Added lombok, hibernate config, object-relation mapping and more

--- 11_hibernate_advanced/src/main/java/com/ormdemo/App.java
+++ 11_hibernate_advanced/src/main/java/com/ormdemo/App.java
@@ -5,6 +5,8 @@ import entity.Employee;
 public class App
 {
     public static void main( String[] args ){
-        Employee e1 = new Employee(101, "Deependra Parmar", 20, 22.5f);
+        Employee e1 = new Employee(101, "Deependra Parmar", 20,"deependraparmar1@gmail.com", 22.5f);
+
+
     }
 }
--- 11_hibernate_advanced/src/main/java/entity/Employee.java
+++ 11_hibernate_advanced/src/main/java/entity/Employee.java
@@ -1,13 +1,31 @@
 package entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
+@Setter
+
+@Table(name = "emp_info")
 public class Employee {
+    @Id
+    @Column(name = "emp_id")
     private int eid;
+
+    @Column(name = "emp_name", nullable = false)
     private String name;
+
+    @Column(name = "emp_age", nullable = false)
     private int age;
+
+    @Column(name = "emp_email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "emp_salary", nullable = true)
     private float salary;
-
-
 }
