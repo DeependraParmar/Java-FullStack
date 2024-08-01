cbcb15699e7a2e5489c82139d7fb87cb458097d0
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-01 09:24:00 +0530

    Minor changes in the SessionFactoryBuilder Class

--- 11_hibernate_advanced/src/main/java/lib/SessionFactoryBuilder.java
+++ 11_hibernate_advanced/src/main/java/lib/SessionFactoryBuilder.java
@@ -7,12 +7,16 @@ import org.hibernate.cfg.Configuration;
 public class SessionFactoryBuilder {
     private static SessionFactory sessionFactory = null;
 
-    static{
+    public static void buildFactory(){
         Configuration configuration = new Configuration().configure();
         sessionFactory = configuration.buildSessionFactory();
     }
 
     public static Session getSession(){
         return sessionFactory.openSession();
+    }
+
+    public static void closeFactory(){
+        sessionFactory.close();
     }
 }
