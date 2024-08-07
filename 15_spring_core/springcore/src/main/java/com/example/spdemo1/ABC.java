d51a009b78e2ca661f31c262f6315c265255700e
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-07 09:14:53 +0530

    Spring Project setup and Bean via Constructor Injection

---
+++ 15_spring_core/springcore/src/main/java/com/example/spdemo1/ABC.java
@@ -0,0 +1,14 @@
+package com.example.spdemo1;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ABC {
+    private int num;
+    private String title;
+    private float value;
+}
