1ff425623399fc507ae77690b661c0bb69053ddc
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-07 10:47:40 +0530

    Done for todays: Spring Core

--- 15_spring_core/springcore/src/main/java/com/example/main/Main.java
+++ 15_spring_core/springcore/src/main/java/com/example/main/Main.java
@@ -4,12 +4,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.example.spdemo1.ABC;
+import com.example.spdemo1.DEF;
+import com.example.spdemo1.PQR;
+import com.example.spdemo1.XYZ;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 //        ABC ob = context.getBean(ABC.class);
-        ABC ob = (ABC) context.getBean("a2");
-        System.out.println(ob);
+//        ABC ob = (ABC) context.getBean("a2");
+//        System.out.println(ob);
+        
+        DEF obj = (DEF) context.getBean("d1");
+        System.out.println(obj);
     }
 }
