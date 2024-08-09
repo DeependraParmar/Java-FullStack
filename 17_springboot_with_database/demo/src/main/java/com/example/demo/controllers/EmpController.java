56bea8fd918a6ce472e2facc77f63ff58600be9b
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-09 10:46:31 +0530

    Harnessing the Repository Interface to cover almost all the patterns for selection

--- 17_springboot_with_database/demo/src/main/java/com/example/demo/controllers/EmpController.java
+++ 17_springboot_with_database/demo/src/main/java/com/example/demo/controllers/EmpController.java
@@ -7,10 +7,15 @@ import com.example.demo.entities.Employee;
 import com.example.demo.repositories.EmployeeRepo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -25,5 +30,41 @@ public class EmpController {
         return new ApiResponse(true, data, "Employee Records");
     }
     
+    @PostMapping("/add")
+    public ApiResponse save(@RequestBody Employee employee) {
+        try{
+            employeeRepo.save(employee); // used to save & update the data;
+            return new ApiResponse(true, null, "Employee Added Successfully..");
+        }
+        catch(Exception e){
+            return new ApiResponse(false, e.getMessage());
+        }
+    }
+    
+    @GetMapping("/{id}")
+    public ApiResponse getEmployeeById(@PathVariable(value = "id") int id) {
+        Optional<Employee> optional = employeeRepo.findById(id);
+        if(optional.isPresent())
+            return new ApiResponse(true, optional.get(), "Employee data fetched..");
+        else
+            return new ApiResponse(false, "Employee not found...");
+    }
+    
+    @GetMapping("/test")
+    public ApiResponse test() {
+        // Object data = employeeRepo.findAll();
+
+        // When you want data on the basis of custom params, you need to write custom query but, you have to get rid of writing queries.
+        // In this case, you have take help of Repository.....
+        // Define abstract methods in the interface and use all of them in the controllers.
+
+        // Object data = employeeRepo.findAllByCity("Indore");
+
+        // Object data = employeeRepo.findAllByAgeGreaterThan(10);
+
+        Object data = employeeRepo.findAllByCityAndAgeGreaterThan("Indore",10);
+
+        return new ApiResponse(true, data, "Testing....");
+    }
     
 }
--- 17_springboot_with_database/demo/src/main/java/com/example/demo/repositories/EmployeeRepo.java
+++ 17_springboot_with_database/demo/src/main/java/com/example/demo/repositories/EmployeeRepo.java
@@ -4,8 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Employee;
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> { // Class & Type of Primary Key
+    List<Employee> findAllByCity(String city);
 
+    Optional<Employee> findByEmail(String email);
+
+    List<Employee> findAllByAgeGreaterThan(Integer age);
+
+    List<Employee> findAllByCityAndAgeGreaterThan(String city, Integer age);
 }
