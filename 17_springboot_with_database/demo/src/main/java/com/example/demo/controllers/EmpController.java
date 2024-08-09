eda3549cd5d4cb4a095423f13c52b4941e80efd3
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-09 09:32:47 +0530

    Getting all the employees from emp_repo

---
+++ 17_springboot_with_database/demo/src/main/java/com/example/demo/controllers/EmpController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Employee;
+import com.example.demo.repositories.EmployeeRepo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/emp")
+public class EmpController {
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @GetMapping("/list")
+    public List<Employee> list() {
+        return (List<Employee>) employeeRepo.findAll();
+    }
+    
+}
